57291302e27540bc41bec6a4e04edbee13b91aae
isaacnfairplay <64980436+isaacnfairplay@users.noreply.github.com>
2023-07-06 15:14:31 -0400

    fixed tests and closed polars issue using np.random.randint made other small changes and now all tests pass https://github.com/pola-rs/polars/issues/9748

--- integers.py
+++ integers.py
@@ -22,8 +22,8 @@ def calculate_int_range(datatype : pl.DataType)-> IntegerRange:
         max_value = 2**(bit_resolution-1)-1
     return IntegerRange(datatype, min_value, max_value, bit_resolution)
 
-type_ranges = {calculate_int_range(type) for type in pl.INTEGER_DTYPES}
-print(type_ranges)
+TYPE_RANGES = frozenset({calculate_int_range(type) for type in pl.INTEGER_DTYPES})
+
 
 
 
@@ -45,7 +45,7 @@ def downcast_integers(df):
         #Get the min and max values of the column
         min_value = df.select(pl.col(col)).min().collect()[col].to_list()[0]
         max_value = df.select(pl.col(col)).max().collect()[col].to_list()[0]
-        compatable_types = (type_range for type_range in type_ranges if type_range.min_value <= min_value and type_range.max_value >= max_value)
+        compatable_types = (type_range for type_range in TYPE_RANGES if type_range.min_value <= min_value and type_range.max_value >= max_value)
         sorted_types = sorted(compatable_types, key=lambda x: x.bit_resolution)
         if not sorted_types:
             continue
@@ -57,44 +57,52 @@ def downcast_integers(df):
     df = df.collect() if start_frame_type == pl.DataFrame else df
     return df
 
-def test_downcast_integers(datatype):
+def test_downcast_integers(datatype, mode= 'eager'):
     import numpy as np
 
     if datatype not in pl.INTEGER_DTYPES:
         return True
-    print(datatype)
+
     #calculate test range
     type_range = calculate_int_range(datatype)
-    #generate test data
-
-    test_data = list( range(type_range.min_value, type_range.max_value, (type_range.max_value-type_range.min_value)//1000))
-    print(len(test_data))
-    #create polars dataframe
+    #generate random 100 test points using Uint64 as part of the numpy function for unsigned datatypes
+    test_data = np.random.randint(type_range.min_value, type_range.max_value, 100, 
+               dtype=np.uint64 if type_range.min_value == 0 else np.int64)
     
     if min(test_data) ==0:
-        df = pl.DataFrame({"test": test_data}).with_columns(pl.col("test").cast(pl.UInt64))
+        df = pl.DataFrame({"test": test_data})
     else: 
-        df = pl.DataFrame({"test": test_data}).with_columns(pl.col("test").cast(pl.Int64))
-    
-    print(df.dtypes)
+        df = pl.DataFrame({"test": test_data})
+        
+    if mode=='lazy':
+        df = df.lazy()
+ 
     #test if downcasted correctly
     downcasted_df = downcast_integers(df)
-    assert downcasted_df["test"].dtype == datatype, f"Downcasted to {downcasted_df['test'].dtype} but should be {datatype}"
+    downcasted_types = downcasted_df.schema
+    
+    assert type(downcasted_df) == type(df), f"Downcasted to {type(downcasted_df)} but should be {type(df)}"
+    assert downcasted_types["test"] == datatype, f"Downcasted to {downcasted_types['test']} but should be {datatype}"
     
 
 def test_downcast_integers_all():
     for type in pl.INTEGER_DTYPES:
-        test_downcast_integers(type)
+        for mode in ['lazy', 'eager']:
+            test_downcast_integers(type, mode=mode)
+    print("All tests passed")
 
 
-def min_reproducable_example():
+
+def _min_rep_example_polars_9748():
+    print(f"min reproducable example for issue: https://github.com/pola-rs/polars/issues/9748")
     import polars as pl
     import traceback
+    import numpy as np
     pl.show_versions()
     min_value = 0 # min value for UInt64
     max_value = 2**64-1 # max value for UInt64
     print(f"Python polars version {pl.__version__}")
-    values = list( range(min_value, max_value, (max_value-min_value)//10))
+    values = np.random.randint(min_value, max_value, 100, dtype=np.uint64 )
     print(f"the max test value is less than the max datatype storage value = {max_value > max(values)}")
     print({type(value) for value in values})
     try:
@@ -108,6 +116,6 @@ def min_reproducable_example():
         traceback.print_exc(limit=10)
         print(e)
     print(df.dtypes)
-    
+
 
 
