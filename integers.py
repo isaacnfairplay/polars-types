d17caf15575878e2a75884a998e7d54acf9a05d0
isaacnfairplay <64980436+isaacnfairplay@users.noreply.github.com>
2023-07-06 13:46:12 -0400

    modified test and also skip unneeded casts

--- integers.py
+++ integers.py
@@ -13,16 +13,18 @@ def calculate_int_range(datatype : pl.DataType)-> IntegerRange:
 
     name = datatype.__name__
     bit_resolution = int(name.lower().split("int")[-1])                   
-    signed = name[0].lower() == "u"
-    if signed:
-        min_value = -2**(bit_resolution-1) #we subtract 1 because we need to account for 0
-        max_value = 2**(bit_resolution-1)-1 
-    else:
+    unsigned = name[0].lower() == "u"
+    if unsigned:
         min_value = 0
         max_value = 2**(bit_resolution)-1
+    else:
+        min_value = -2**(bit_resolution-1) 
+        max_value = 2**(bit_resolution-1)-1
     return IntegerRange(datatype, min_value, max_value, bit_resolution)
 
 type_ranges = {calculate_int_range(type) for type in pl.INTEGER_DTYPES}
+print(type_ranges)
+
 
 
 
@@ -41,16 +43,48 @@ def downcast_integers(df):
         if schema_dict[col] not in pl.INTEGER_DTYPES:
             continue
         #Get the min and max values of the column
-        min_value = df.select(pl.col(col)).min().first().collect()[0]
-        max_value = df.select(pl.col(col)).max().first().collect()[0]
-        type_ranges = (type_range for type_range in type_ranges if type_range.min_value <= min_value and type_range.max_value >= max_value)
-        selected_type = sorted(type_ranges, key=lambda x: x.bit_resolution)[0]
-        if not type_ranges:
+        min_value = df.select(pl.col(col)).min().collect()[col].to_list()[0]
+        max_value = df.select(pl.col(col)).max().collect()[col].to_list()[0]
+        compatable_types = (type_range for type_range in type_ranges if type_range.min_value <= min_value and type_range.max_value >= max_value)
+        sorted_types = sorted(compatable_types, key=lambda x: x.bit_resolution)
+        if not sorted_types:
             continue
-        expressions.append(selected_type.col(col).cast(selected_type.datatype))
+        selected_type = sorted_types[0]
+        if selected_type.datatype == schema_dict[col]:
+            continue
+        expressions.append(pl.col(col).cast(selected_type.datatype))
     df = df.with_columns(expressions) if expressions else df
     df = df.collect() if start_frame_type == pl.DataFrame else df
     return df
 
+def test_downcast_integers(datatype):
+    import numpy as np
 
+    if datatype not in pl.INTEGER_DTYPES:
+        return True
+    print(datatype)
+    #calculate test range
+    type_range = calculate_int_range(datatype)
+    #generate test data
+
+    test_data = list( range(type_range.min_value, type_range.max_value, (type_range.max_value-type_range.min_value)//1000))
+    print(len(test_data))
+    #create polars dataframe
     
+    if min(test_data) ==0:
+        df = pl.DataFrame({"test": test_data}).with_columns(pl.col("test").cast(pl.UInt64))
+    else: 
+        df = pl.DataFrame({"test": test_data}).with_columns(pl.col("test").cast(pl.Int64))
+    
+    print(df.dtypes)
+    #test if downcasted correctly
+    downcasted_df = downcast_integers(df)
+    assert downcasted_df["test"].dtype == datatype, f"Downcasted to {downcasted_df['test'].dtype} but should be {datatype}"
+    
+
+def test_downcast_integers_all():
+    for type in pl.INTEGER_DTYPES:
+        test_downcast_integers(type)
+test_downcast_integers_all()
+
+
