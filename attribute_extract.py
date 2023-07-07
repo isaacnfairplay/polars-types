3bbcb6cfbedab39d622522ea2f4df2ed136536a6
isaacnfairplay <64980436+isaacnfairplay@users.noreply.github.com>
2023-07-06 23:01:25 -0400

    Refactored both extract and identify attr methods tests are good non-test code is fairly limited changed from transpose to just a 1 row dataframe

--- attribute_extract.py
+++ attribute_extract.py
@@ -7,50 +7,28 @@ def identify_static_cols(df: pl.DataFrame | pl.LazyFrame)-> list[str]:
     Identify columns that are static attributes
     """
     # loop through df.columns and identify columns that are static
-    static_cols = []
-    for col in df.columns:
-        #This should work for lazy and eager frames
-        column_frame = df.select(pl.col(col).n_unique())
-        if not isinstance(df, pl.DataFrame):
-            column_frame = column_frame.collect()
+    df =df.lazy()
+    return [col for col in df.columns if df.select(pl.col(col).n_unique()).collect()[col].to_list()[0]==1]    
 
-        if column_frame[col].to_list()[0] == 1:
-            static_cols.append(col)
-    return static_cols
-
-def remove_static_cols(df: pl.DataFrame | pl.LazyFrame)-> dict[str, pl.DataFrame | pl.LazyFrame, pl.DataFrame | pl.LazyFrame]:
+def extract_static_cols(df: pl.DataFrame | pl.LazyFrame)-> dict[str, pl.DataFrame | pl.LazyFrame, pl.DataFrame | pl.LazyFrame]:
     """
     Remove columns that are static attributes
     """
     static_cols = identify_static_cols(df)
     attr_df = df.select(static_cols)
-    attr_df = attr_df.select(pl.all().unique())
+    attributes = attr_df.select(pl.all().unique()) #get the unique values of each column
     if not isinstance(attr_df, pl.DataFrame):
-        attr_df = attr_df.collect()
+        attributes = attr_df.collect()
     
     # get the static values of each of the attr_df columns
     #use select to get all the values of each column
-    
-    static_values_lists = [attr_df.select(col)[col].to_list()  for col in attr_df.columns]
-
-    if {col for col in static_values if len(set(col)) != 1}:
-        raise ValueError("Static columns with unique values are not all static - see {attr_df.distinct()}")
-    
-    static_values = [static_values[col][0] for col in static_values_lists]
-
-    attributes = pl.DataFrame({
-        'column_name': 
-        static_cols, 
-        'value': 
-            static_values,
-        'data_type':
-            [attr_df.schema[col] for col in static_cols]
-            })
-    
     data = df.select([col for col in df.columns if col not in static_cols])
     
     return {"data": data, "attributes": attributes}
-        
+
+
+#TESTS
+  
 def test_identify_static_cols():
     test_frame = pl.DataFrame({
         "a": [1,1,1,1,1,1,1,1,1,1], 
@@ -86,5 +64,43 @@ def test_identify_static_cols():
     print("Test passed")
         
 #make call to test func
-test_identify_static_cols()
+def test_extract_static_cols():
+    test_frame = pl.DataFrame({
+        "a": [1,1,1,1,1,1,1,1,1,1], 
+        "b": [1,1,1,1,1,1,1,1,1,1],
+        "c": [1,1,1,1,1,1,1,1,1,1],
+        'x': [1,2,3,4,5,6,7,8,9,10],
+        #string columns
+        "d": ["a","a","a","a","a","a","a","a","a","a"],
+        "e": ["a","a","a","a","a","a","a","a","a","a"],
+        "ecat": ["a","a","a","a","a","a","a","a","a","a"],
+        #float columns
+        "f": [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        "g": [1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        #bool columns
+        'h': [True, True, True, True, True, True, True, True, True, True],
+        'i': [True, True, True, True, True, True, True, True, True, True],
+        'j': [True, True, True, False, True, True, True, True, True, True]
+    })
+    # Convert one string column to categorical
+    test_frame = test_frame.with_columns(pl.col("ecat").cast(pl.Categorical))
+    #Test function
+    test_values = ('a', 'b', 'c', 'd', 'e', 'ecat', 'f', 'h', 'i')
+    static_cols = extract_static_cols(test_frame)
+    comparison_attributes = pl.DataFrame({
+        "a": [1],
+        "b": [1],
+        "c": [1],
+        "d": ["a"],
+        "e": ["a"],
+        "ecat": ["a"],
+        "f": [1.0],
+        "h": [True],
+        "i": [True]
+    })
     
+    comparison_data = test_frame.select([col for col in test_frame.columns if col not in test_values])
+    
+    assert static_cols["attributes"].frame_equal(comparison_attributes), f"attributes not equal"
+    assert static_cols["data"].frame_equal(comparison_data), f"data not equal"
+    print("Test passed")
