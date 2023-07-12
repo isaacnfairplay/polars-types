d7b425cf86ca55e59e625029ae9551a737026d23
isaacnfairplay <64980436+isaacnfairplay@users.noreply.github.com>
2023-07-12 16:41:56 -0400

    Create compress.py

---
+++ compress.py
@@ -0,0 +1,14 @@
+import polars as pl
+from integers import downcast_integers
+from strings import downcast_strings
+
+def compresss_frame(df):
+    df = downcast_strings(df)
+    df = downcast_integers(df)
+    return df
+
+def real_test_compresss_file():
+    df = pl.scan_csv("exampledata1.csv", infer_schema_length=1)
+    print(f"Original schema: {df.schema}")
+    df = compresss_file(df).collect()
+    print(f"Schema: {df.schema}")
