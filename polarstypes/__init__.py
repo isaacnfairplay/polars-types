1babdec6203ea461337066bf0ef4d6a8754521f2
isaac_moore <isaac.moore.pq@hitachiastemo.com>
2023-07-12 17:01:52 -0400

    .module syntax to indicate modules in same folder

--- polarstypes/__init__.py
+++ polarstypes/__init__.py
@@ -1,7 +1,7 @@
 import polars as pl
-from integers import downcast_integers
-from strings import downcast_strings
-from compress import compress_frame
+from .integers import downcast_integers
+from .strings import downcast_strings
+from .compress import compress_frame
 
 
 #real_test_compresss_file()
