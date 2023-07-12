248c366b41e50c1fca6f31c276c5a9a3f83eb577
isaacnfairplay <64980436+isaacnfairplay@users.noreply.github.com>
2023-07-12 16:39:56 -0400

    Update __init__.py
    removing function definitions to different module

--- __init__.py
+++ __init__.py
@@ -1,16 +1,7 @@
 import polars as pl
 from integers import downcast_integers
 from strings import downcast_strings
+from compress import compress_frame
 
-def compresss_file(df):
-    df = downcast_strings(df)
-    df = downcast_integers(df)
-    return df
-
-def real_test_compresss_file():
-    df = pl.scan_csv("exampledata1.csv", infer_schema_length=1)
-    print(f"Original schema: {df.schema}")
-    df = compresss_file(df).collect()
-    print(f"Schema: {df.schema}")
 
 #real_test_compresss_file()
